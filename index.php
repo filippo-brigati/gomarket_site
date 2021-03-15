75e96290a5e14513f8f05f04c2767757ac0cc8ed
Filippo Brigati <filippobrigati2@gmail.com>
2021-03-15 22:11:48 +0100

    new_home_ui

--- index.php
+++ index.php
@@ -108,8 +108,93 @@
             </nav>
         ";
         $body = "
+            <div class='row d-flex align-items-center pt-4'>
+                <div class='col-md-5'>
+                    <h1>Benvenuto su Gomarket! Pronto ad Ordinare la tua prossima spesa?!</h1>
+                    <div class='pt-3'><a href='./page/login.php' type='button' class='btn btn-outline-primary btn-lg'>Ordina Ora</a></div>
+                </div>
+                <div class='col-lg-1'></div>
+                <div class='col-md-6'>
+                <img src='./assets/10.svg' class='img-fluid mx-auto d-block'>
+                </div>
+            </div>
+            <div class='container row d-flex align-items-center pt-4'>
+                <div class='col-md-4'>
+                    <div class='card border-light mb-3'>
+                        <img src='./assets/11.svg' class='card-img-top' alt='...'>
+                        <div class='card-body'>
+                            <h4 class='text-center'>VELOCE</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-md-4'>
+                    <div class='card border-light mb-3'>
+                        <img src='./assets/12.svg' class='card-img-top' alt='...'>
+                        <div class='card-body'>
+                            <h4 class='text-center'>SICURO</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class='col-md-4'>
+                    <div class='card border-light mb-3'>
+                        <img src='./assets/13.svg' class='card-img-top' alt='...'>
+                        <div class='card-body'>
+                            <h4 class='text-center'>SEMPLICE</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
         ";
     }
+
+    $footer = "
+    <footer class='text-center text-lg-start' style='background-color: rgb(102, 106, 246)'>
+      <div class='container p-4'>
+        <div class='container row text-light'>
+          <div class='col-lg-6 col-md-12 mb-4 mb-md-0'>
+            <h5 class='text-uppercase'>Footer Content</h5>
+            <p>
+                Ciao!
+            </p>
+          </div>
+          <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+            <h5 class='text-uppercase'>Links</h5>
+            <ul class='list-unstyled mb-0'>
+              <li>
+                <a href='#!' class='text-dark'>Link 1</a>
+              </li>
+              <li>
+                <a href='#!' class='text-dark'>Link 2</a>
+              </li>
+              <li>
+                <a href='#!' class='text-dark'>Link 3</a>
+              </li>
+              <li>
+                <a href='#!' class='text-dark'>Link 4</a>
+              </li>
+            </ul>
+          </div>
+          <div class='col-lg-3 col-md-6 mb-4 mb-md-0'>
+            <h5 class='text-uppercase mb-0'>Links</h5>
+            <ul class='list-unstyled'>
+              <li>
+                <a href='#!' class='text-dark'>Link 1</a>
+              </li>
+              <li>
+                <a href='#!' class='text-dark'>Link 2</a>
+              </li>
+              <li>
+                <a href='#!' class='text-dark'>Link 3</a>
+              </li>
+              <li>
+                <a href='#!' class='text-dark'>Link 4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+    ";
 
     $html = "
         <html>
@@ -123,10 +208,9 @@
                 <title>GoMarket</title>
             </head>
             <body>
-                <div class='container-lg'>
-                    {$nav}
-                    {$body}
-                </div>
+                <div class='container'>{$nav}</div>
+                <div class='container'><div class='container'>{$body}</div></div>
+                {$footer}
                 <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js' integrity='sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0' crossorigin='anonymous'></script>
             </body>
         </html>    
