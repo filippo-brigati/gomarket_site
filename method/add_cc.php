46697cbeabfeec9989d45ed41f48e9c3a44baba4
Filippo Brigati <filippobrigati2@gmail.com>
2021-04-21 18:53:31 +0200

    ADD: crypto_card

--- method/add_cc.php
+++ method/add_cc.php
@@ -3,14 +3,14 @@
     include('./funzioni.php');
 
     $data_scadenza = $_POST['d_scad'];
-    $last_nb = substr($_POST['n_card'],12);
+    $last_nb = substr($_POST['n_card'], 12);
     
     if(isset($_POST['remember'])) {
         $numero_carta = password_hash($_POST['n_card'], PASSWORD_DEFAULT);
         $cv = password_hash($_POST['cv'], PASSWORD_DEFAULT);
 
         $sql = "INSERT INTO carta_credito (numero_carta, data_scadenza, cv, last_nb, fk_id_utente) 
-        VALUES ('$numero_carta', '$data_scadenza', $cv, $last_nb, {$_SESSION["ID_CLIENTE"]})";
+        VALUES ('$numero_carta', '$data_scadenza', '$cv', '$last_nb', {$_SESSION["ID_CLIENTE"]})";
         
         inserisci($sql, "gomarket");
     }
